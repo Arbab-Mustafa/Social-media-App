52500b76aa36a7bb7879da3396ea1902db2c9f93
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-11-13 21:16:36 +0500

    add new thing

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import { AuthProvider } from "@/context/AuthContext";
+// import { AuthProvider } from "@/context/AuthContext";
 import { QueryProvider } from "@/lib/react-query/QueryProvider";
 
 import App from "./App";
@@ -11,9 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
+        {/* <AuthProvider> */}
+        <App />
+        {/* </AuthProvider> */}
       </QueryProvider>
     </BrowserRouter>
   </React.StrictMode>
